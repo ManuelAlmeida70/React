86cfc193e5f9083765560d46f4bd42997dde2dd4
Manuel de Almeida <ManuelAlmeida70@outlook.pt>
2025-05-28 17:32:09 +0100

    done

--- src/pages/inputs/inputs.tsx
+++ src/pages/inputs/inputs.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header";
+import { constants } from "fs/promises";
 import { useState } from "react";
+
+const coursesList = ["Html", "React", "Javascript", "Arduino", "Css"];
+
 
 export default function inputs()
 {
 	const [name, setName] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+	const [courses, setCourse] = useState<string>("");
 
 	return (
 		<div>
@@ -12,6 +17,16 @@ export default function inputs()
 			<div className="fieldForm">
 				<label>Nome</label>
 				<input type="text" placeholder="your name" value={name} onChange={(events)=>setName(events.target.value)}/>
+			</div>
+			<div className="fieldForm">
+				<label>Course:</label>
+				<select name="course" id="course" value={courses} onChange={(evt)=>setCourse(evt.target.value)}>
+					{
+						coursesList.map((c:any)=>{
+							return <option value={c}>{c}</option>;
+						})
+					}
+				</select>
 			</div>
 			<div className="fieldForm">
 				<label>Senha</label>
